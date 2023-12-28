65234d500d999ab5682907ff6535772db551c099
Brinda Moudgalya <brindamoudgalya05@gmail.com>
2023-12-28 15:57:44 -0800

    edited diary, need to fix data saving fxn.

--- mlh december hackathon/pages/1_personal_diary.py
+++ mlh december hackathon/pages/1_personal_diary.py
@@ -6,28 +6,41 @@ from datetime import datetime
 from streamlit_option_menu import option_menu
 
 
-def main():
+def main(): 
 
     st.title("Personal Diary :notebook:")
 
     # change view based on what menu button user clicks
+
+    selected = option_menu(
+            menu_title=None,
+            options=["Today", "Browse Old Entries"],
+            orientation='horizontal'
+    )
+
     today_date = datetime.now().strftime("%m/%d/%Y")
-    menu = ["Today", "Browse Old Entries"]
-    choice = st.sidebar.selectbox("Which entry/entries would you like to visit?", menu)
-    if choice == "Today":
-        # layout:
+
+    if selected=="Today":
         st.text_area("Today's Entry (" + today_date + "):")
-        
         if st.button("Save"):
             # generate success message:
             success_message = st.success("Saved.")
             time.sleep(2) # wait 2 seconds
             success_message.empty()
+    elif selected=="Browse Old Entries":
+        col1, col2 = st.columns(2)
+        current_mood="test"
+        # setting what is in each column on page:  
+        with col1:
+            st.caption(":gray[date:]")
+            # CHANGE NAME OF BUTTON TO BE DATES WHEN AN ENTRY IS CREATED.
+            if st.button(datetime.now().strftime("%m/%d/%Y")):
+                st.markdown("hello")
+        with col2:
+            st.caption(":gray[mood:]")
+            st.markdown(current_mood)
 
 
-    elif choice == "Browse Old Entries":
-        # layout:
-        date = st.date_input("date: ", value="today", format="MM/DD/YYYY")
 
 if __name__ == '__main__':
     main()
